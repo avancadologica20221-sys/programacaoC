9ed7f2de844a267fdb03c01898883cebba5eef90
avancadologica20221-sys <avancado.logica.2022.1@gmail.com>
2025-10-27 16:00:26 -0400

    Add density and PIB per capita calculations
    Added calculations for density and PIB per capita for both cartas. Incluindo o nível intermediário das atividades

--- super_trunfo.c
+++ super_trunfo.c
@@ -4,7 +4,7 @@
 		
 		char estado1[20], estado2[20], codigo1[20], codigo2[20], cidade1[50], cidade2[50];
 		int populacao1, populacao2, pTuristico1,  pTuristico2;
-		float area1, pib1, area2, pib2;
+		float area1, pib1, area2, pib2, densidade1, pib_percapita1, densidade2, pibpercapita2;
 		
 				
 		printf("Cadastro de Cartas \n");
@@ -42,6 +42,7 @@
 		scanf("%d", &pTuristico2);	
 		
 		printf("Veja como ficou o cadastramento \n");
+		
 		printf("Carta1 \n");
 		printf("Estado: %s \n", estado1);
 		printf("Codigo do estado: %s \n", codigo1);
@@ -51,6 +52,15 @@
 		printf("PIB: %2.f \n", pib1);
 		printf("Numero de Pontos Turisticos: %d \n", pTuristico1);
 		
+	    /*densidade = população/area
+		pib_percapita = pib/população'/
+
+		densidade1 = (float) populacao1 / area1;	
+		pib_percapita = (float) pib1 / populacao1;
+
+		printf("Densidade: %.2f \n", densidade1);
+		printf("Pib per capita: %.2f \n. pib_percapita1);
+		
 		printf("Carta 2 \n");
 		printf("Estado: %s \n", estado2);
 		printf("Codigo do estado: %s \n", codigo2);
@@ -59,6 +69,12 @@
 		printf("Area: %2.f \n", area2);
 		printf("PIB: %2.f \n", pib2);
 		printf("Numero de Pontos Turisticos: %d \n", pTuristico2);
-				
+
+		densidade2 = (float) populacao2 / area2;	
+		pib_percapita = (float) pib2 / populacao2;
+
+		printf("Densidade: %.2f \n", densidade2);
+		printf("Pib per capita: %.2f \n. pib_percapita2);
+		
 		return 0;
 	}
